107c108cec10fbf3b4df128b159ba820f9967474
karananu <karanrana3095@gmail.com>
2023-04-29 14:42:06 +0530

    first commit

---
+++ chatGpt/src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import {logo } from "../assets"
+
+const Hero = () => {
+  return (
+    <header className="w-full flex
+    justify-center items-center flex-col ">
+      <nav className="flex justify-between item-center w-full mb-10 pt-3">
+        <img src={logo} alt="somelogo" 
+        className="w-28 object-contain"/>
+        <button
+        type="button"
+        onClick={()=>{
+          window.open('https://github.com/karananu')
+        }}
+        className="black_btn">Github</button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="mx-md:hidden"/>
+        <span className="orange_gradient">Open Ai GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summarize , an 
+        open-source article summarizer that transforms lengthy articles 
+        into clear and concise sumamrizer.
+      </h2>
+
+    </header>
+  )
+}
+
+export default Hero
